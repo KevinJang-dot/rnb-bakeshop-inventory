306ba5d31de8371068858f46d4e72cf297c739c8
KevinJang-dot <jangkevinjayd@gmail.com>
2026-09-11 12:12:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,10 @@ const firebaseConfig = {
 // INITIALIZE FIREBASE
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
+const db = getFirestore(app);
 
 // GET HTML ELEMENTS
 
